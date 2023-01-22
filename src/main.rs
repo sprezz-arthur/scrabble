1c53bf30c5bb15b4ae77487c1368224e64002e08
Arthur <120720568+sprezz-arthur@users.noreply.github.com>
2023-01-22 11:17:58 +0100

    add dummy tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+fn ask_name() -> String {
+    use std::io;
+
+    println!("What is your name?");
+    let mut name = String::new();
+
+    io::stdin()
+        .read_line(&mut name)
+        .expect("failed to readline");
+
+    return name.trim().to_string();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let name: String = ask_name();
+    println!("Hello, {}!", name.trim());
 }
