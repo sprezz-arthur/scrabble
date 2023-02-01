2f9b22af6569891333ce1a5eb3e1d4dbc7bc38c6
Arthur <120720568+sprezz-arthur@users.noreply.github.com>
2023-02-01 11:31:16 +0100

    add letters

---
+++ src/letter/tests.rs
@@ -0,0 +1,17 @@
+use crate::letter::{LETTER_DISTRIBUTION, LETTER_VALUES};
+
+
+#[test]
+fn total_letters_count(){
+    let total_count: i32 = LETTER_DISTRIBUTION.map(|l| l.1).iter().sum();
+    assert_eq!(total_count, 100);
+}
+
+#[test]
+fn total_letters_value(){
+    let mut total_value = 0;
+    for (count, value) in LETTER_DISTRIBUTION.iter().zip(LETTER_VALUES.iter()){
+        total_value += count.1*value.1;
+    }
+    assert_eq!(total_value, 187);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod letter;
 mod bag;
 mod board;
 mod dictionary;
